72340dc2f5941a7357eb14ff253e44ff3f33f010
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-10-26 21:19:20 -0400

    finished the assignment

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // _________________________________________________________________
 
 
-// PART ONE: Getting Started
+// Getting Started
 
 const mainEl = document.querySelector("main");
 mainEl.style.height = "100vh"
@@ -13,28 +13,46 @@ h1El.textContent = "DOM Manipulation";
 mainEl.appendChild(h1El);
 mainEl.style.backgroundColor = "#4a4e4d";
 
-// PART TWO: Creating a Menu Bar
+// Creating a Menu Bar
 
 const topMenuEl = document.querySelector("#top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "#0e9aa7";
 topMenuEl.classList.add("flex-around");
 
-// PART THREE: Adding Menu Buttons
+// Adding Menu Buttons
 
 // Menu data structure
 var menuLinks = [
-    { text: 'about', href: '/about' },
-    { text: 'catalog', href: '/catalog' },
-    { text: 'orders', href: '/orders' },
-    { text: 'account', href: '/account' },
+  { text: 'about', href: '/about' },
+  {
+    text: 'catalog', href: '#', subLinks: [
+      { text: 'all', href: '/catalog/all' },
+      { text: 'top selling', href: '/catalog/top' },
+      { text: 'search', href: '/catalog/search' },
+    ]
+  },
+  {
+    text: 'orders', href: '#', subLinks: [
+      { text: 'new', href: '/orders/new' },
+      { text: 'pending', href: '/orders/pending' },
+      { text: 'history', href: '/orders/history' },
+    ]
+  },
+  {
+    text: 'account', href: '#', subLinks: [
+      { text: 'profile', href: '/account/profile' },
+      { text: 'sign out', href: '/account/signout' },
+    ]
+  },
 ];
 
 menuLinks.forEach(link => {
-    const anchorTag = document.createElement("a");
-    anchorTag.href = link.href;
-    anchorTag.textContent = link.text;
-    topMenuEl.appendChild(anchorTag);
+  const anchorTag = document.createElement("a");
+  anchorTag.href = link.href;
+  anchorTag.textContent = link.text;
+  topMenuEl.appendChild(anchorTag);
+
 });
 
 // _________________________________________________________________
@@ -43,13 +61,57 @@ menuLinks.forEach(link => {
 // DOM Manipulation Part 2
 
 
-// PART THREE: Creating the Submenu
+// Creating the Submenu
 
 const subMenuEl = document.querySelector("#sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "#3da4ab";
-subMenuEl.classList.add = "flex-around";
-subMenuEl.style.position ="absolute";
-subMenuEl.style.top ="0";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+// Adding Menu Interaction
+
+const topMenuLinks = topMenuEl.querySelectorAll("a");
+
+topMenuEl.addEventListener('click',
+
+  function (event) {
+    event.preventDefault();
+    if (event.target.nodeName === 'A') {
+      console.log(event.target.textContent);
+
+
+      if (event.target.nodeName === 'A') {
+        const clickedLink = event.target;
+        console.log(clickedLink.textContent);
+
+        if (!clickedLink.classList.contains('active')) {
+          clickedLink.classList.add('active');
+          subMenuEl.innerHTML = " ";
+          menuLinks.forEach(el => {
+            if (el.text == clickedLink.textContent) {
+              el.subLinks.forEach(sub => {
+                let subMenuItem = document.createElement("a");
+                subMenuItem.textContent = sub.text;
+                subMenuEl.append(subMenuItem);
+              })
+            } else {
+              subMenuEl.style.top = '0';
+            }
+          })
+        }
+        const menuLink = menuLinks.find(link => link.text.toLowerCase() === clickedLink.textContent.toLowerCase());
+
+        if (menuLink && menuLink.subLinks) {
+          subMenuEl.style.top = '100%';
+
+        }
+      }
+
+    }
+  })
+
+
 
 
